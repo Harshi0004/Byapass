995d5163fb59128321efd9ac49da9f01e0df9605
Harsha <164939644+Harshi0004@users.noreply.github.com>
2024-05-10 22:28:53 +0530

    Update bypass_ddl.py

--- FZBypass/core/bypass_ddl.py
+++ FZBypass/core/bypass_ddl.py
@@ -235,7 +235,7 @@ async def transcript(url: str, DOMAIN: str, ref: str, sltime) -> str:
 async def shareus(url):
     code = url.split('/')[-1]
     DOMAIN = "https://api.shrslink.xyz"
-    headers = {'User-Agent':'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36', 'Origin':'https://shareus.io'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36', 'Origin': 'https://shareus.io'}
     api = f"{DOMAIN}/v?shortid={code}&initial=true&referrer="
     async with ClientSession() as session:
         async with session.get(api, headers=headers) as resp:
