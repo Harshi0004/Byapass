32a2fdfbfbb83e5ffc12494a9dbd7d5bc79f6d24
Harsha <164939644+Harshi0004@users.noreply.github.com>
2024-05-11 10:21:50 +0530

    Update bypass_ddl.py

--- FZBypass/core/bypass_ddl.py
+++ FZBypass/core/bypass_ddl.py
@@ -243,14 +243,23 @@ async def shareus(link):
             return r
 
 def shrs(link):
-    r = link.replace("https://shrs.link/","")
-    response = requests.get("https://api.shrslink.xyz/v?shortid="+r+"&initial=true&referrer=").json()
-    if response.get('sid'):
-        sid = response['sid']
-        response = requests.get("https://api.shrslink.xyz/get_link?sid="+sid).json()
-        if response.get('link_info') and response['link_info'].get('destination'):
-            return response['link_info']['destination']
-    return "Failed to bypass link"
+   code = url.split('/')[-1]
+    DOMAIN = "https://api.shrslink.xyz"
+    headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36', 'Origin': 'https://shareus.io'}
+    api = f"{DOMAIN}/v?shortid={code}&initial=true&referrer="
+    async with aiohttp.ClientSession() as session:
+        async with session.get(api, headers=headers) as resp:
+            data = await resp.json()
+            id = data.get('sid')
+            if not id:
+                return "ID Error"
+            else:
+                api_2 = f"{DOMAIN}/get_link?sid={id}"
+                async with session.get(api_2, headers=headers) as resp_2:
+                    data_2 = await resp_2.json()
+                    final = data_2['link_info']['destination']
+                    return final
+
 
 def main():
     link = input("Enter The Shareus Link :")
@@ -288,25 +297,7 @@ async def dropbox(url: str) -> str:
         .replace("dropbox.com", "dl.dropboxusercontent.com")
         .replace("?dl=0", "")
     )
-
-async def shrs(link):
-    code = url.split('/')[-1]
-    DOMAIN = "https://api.shrslink.xyz"
-    headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36', 'Origin': 'https://shareus.io'}
-    api = f"{DOMAIN}/v?shortid={code}&initial=true&referrer="
-    async with aiohttp.ClientSession() as session:
-        async with session.get(api, headers=headers) as resp:
-            data = await resp.json()
-            id = data.get('sid')
-            if not id:
-                return "ID Error"
-            else:
-                api_2 = f"{DOMAIN}/get_link?sid={id}"
-                async with session.get(api_2, headers=headers) as resp_2:
-                    data_2 = await resp_2.json()
-                    final = data_2['link_info']['destination']
-                    return final
-
+    
 async def linkvertise(url: str) -> str:
     resp = rget("https://bypass.pm/bypass2", params={"url": url}).json()
     if resp["success"]:
