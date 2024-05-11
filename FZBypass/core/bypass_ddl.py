1db46c75d590e5a5ac0836916ff471628d8b123e
Harsha <164939644+Harshi0004@users.noreply.github.com>
2024-05-11 10:18:53 +0530

    Update bypass_ddl.py

--- FZBypass/core/bypass_ddl.py
+++ FZBypass/core/bypass_ddl.py
@@ -289,6 +289,23 @@ async def dropbox(url: str) -> str:
         .replace("?dl=0", "")
     )
 
+async def shareus(url):
+    code = url.split('/')[-1]
+    DOMAIN = "https://api.shrslink.xyz"
+    headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36', 'Origin': 'https://shareus.io'}
+    api = f"{DOMAIN}/v?shortid={code}&initial=true&referrer="
+    async with aiohttp.ClientSession() as session:
+        async with session.get(api, headers=headers) as resp:
+            data = await resp.json()
+            id = data.get('sid')
+            if not id:
+                return "ID Error"
+            else:
+                api_2 = f"{DOMAIN}/get_link?sid={id}"
+                async with session.get(api_2, headers=headers) as resp_2:
+                    data_2 = await resp_2.json()
+                    final = data_2['link_info']['destination']
+                    return final
 
 async def linkvertise(url: str) -> str:
     resp = rget("https://bypass.pm/bypass2", params={"url": url}).json()
